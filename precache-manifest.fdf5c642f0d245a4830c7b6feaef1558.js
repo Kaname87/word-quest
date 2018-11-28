8c9bccb360a6b1ed51b3fc9497e958b0b15a3ca7
kaname87 <kaname.nakanishi@bibo.com.ph>
2018-11-28 14:16:39 +0800

    Updates

--- precache-manifest.fdf5c642f0d245a4830c7b6feaef1558.js
+++ precache-manifest.fdf5c642f0d245a4830c7b6feaef1558.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/word-quest/static/js/runtime~main.3f5e2a9e.js"
   },
   {
-    "revision": "c6b36860141d8510722c",
-    "url": "/word-quest/static/js/main.c6b36860.chunk.js"
+    "revision": "c5fce23e9851294fc254",
+    "url": "/word-quest/static/js/main.c5fce23e.chunk.js"
   },
   {
     "revision": "8fb7e30065dd2cb14ed9",
     "url": "/word-quest/static/js/1.8fb7e300.chunk.js"
   },
   {
-    "revision": "c6b36860141d8510722c",
+    "revision": "c5fce23e9851294fc254",
     "url": "/word-quest/static/css/main.dea4ad9c.chunk.css"
   },
   {
-    "revision": "121882459ef0304fbadc0fd3f502fb9b",
+    "revision": "5bd00b4a4ab4bb1b5f27fdfa515bf294",
     "url": "/word-quest/index.html"
   }
 ];
